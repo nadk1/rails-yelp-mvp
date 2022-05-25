f503bd4c04bc793c582360219d2efbde06215f26
nadk1 <nomadic.nad@icloud.com>
2022-05-25 13:01:34 +0400

    Completed Restaurants part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Bella's", address: "3 Via Morgana, Little Italy", phone_number: "36308723", category: "italian")
+Restaurant.create(name: "Ching", address: "72 Chong Street, China Town", phone_number: "52795023", category: "chinese")
+Restaurant.create(name: "Arso", address: "49 Sony Street, Animeland", phone_number: "56028948", category: "japanese")
+Restaurant.create(name: "Nik Tamer", address: "69 Rue des Archives, État Libre d'Orange", phone_number: "70583727", category: "french")
+Restaurant.create(name: "Roi des Belges", address: "2 Leopold, Congo", phone_number: "60810858", category: "belgian")
